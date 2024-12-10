e539680f3aa9385cfa059804c0d307df16f7b8e1
Ramairo <132521404+Ramairo@users.noreply.github.com>
2024-12-10 14:56:10 -0300

    Actividad 1

---
+++ Actividad 1/main.js
@@ -0,0 +1,28 @@
+function calcularMonto() {
+    const monto = parseFloat(document.getElementById('monto').value);
+    const medioPago = document.getElementById('pago').value;
+  
+    if (isNaN(monto) || monto <= 0) {
+      alert("Por favor, ingrese un monto válido.");
+      return;
+    }
+  
+    const resultado = facturacion(monto, medioPago);
+    document.getElementById('resultado').textContent = `Resultado: $${resultado.toFixed(2)}`;
+  }
+  
+  function facturacion(monto, medioPago) {
+    if (monto < 200) return monto;
+  
+    let descuento = 0;
+    if (monto > 400) {
+      descuento = 0.4;
+    } else {
+      if (medioPago === 'E') descuento = 0.3;
+      else if (medioPago === 'D') descuento = 0.2;
+      else if (medioPago === 'C') descuento = 0.1;
+    }
+  
+    return monto * (1 - descuento);
+  }
+  
