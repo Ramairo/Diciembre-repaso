771635314dd4ecaf7b653be51f2885c8670bb231
Ramairo <132521404+Ramairo@users.noreply.github.com>
2024-12-15 22:00:48 -0300

    Update main.js

--- Actividad 1/main.js
+++ Actividad 1/main.js
@@ -1,28 +1,28 @@
-function calcularMonto() {
-    const monto = parseFloat(document.getElementById('monto').value);
-    const medioPago = document.getElementById('pago').value;
-  
+function calcularMonto(montoInput, medioPagoInput, resultadoElement) {
+    const monto = parseFloat(montoInput.value);
+    const medioPago = medioPagoInput.value;
+
     if (isNaN(monto) || monto <= 0) {
-      alert("Por favor, ingrese un monto válido.");
-      return;
+        alert("Por favor, ingrese un monto válido.");
+        return;
     }
-  
+
     const resultado = facturacion(monto, medioPago);
-    document.getElementById('resultado').textContent = `Resultado: ${resultado}`;
-  }
-  
-  function facturacion(monto, medioPago) {
+    resultadoElement.textContent = `Resultado: ${resultado}`;
+}
+
+function facturacion(monto, medioPago) {
     if (monto < 200) return monto;
-  
+
     let descuento = 0;
     if (monto > 400) {
-      descuento = 0.4;
+        descuento = 0.4;
     } else {
-      if (medioPago === 'E') descuento = 0.3;
-      else if (medioPago === 'D') descuento = 0.2;
-      else if (medioPago === 'C') descuento = 0.1;
+        if (medioPago === 'E') descuento = 0.3;
+        else if (medioPago === 'D') descuento = 0.2;
+         else if (medioPago==='MP') descuento = 0.15; 
+        else if (medioPago === 'C') descuento = 0.1;
     }
-  
+
     return monto * (1 - descuento);
-  }
-  
+}
